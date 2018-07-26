14c2f22f04d9dc6c22081ee4ce69af3cea036c50
Mike Ferrara <mikef@mrf.sonoma.ca.us>
2018-07-26 11:28:18 -0700

    File writer

--- cmd/solarmon/main.go
+++ cmd/solarmon/main.go
@@ -5,8 +5,18 @@ import (
 	"fmt"
 	"github.com/mikef5410/solarmon"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"time"
 )
+
+type LiveData struct {
+	InverterData       solarmon.PerfData
+	GridData           solarmon.DataResponse
+	InverterEfficiency float64
+	HousePowerUsage    float64
+	TimeStamp          int64
+}
 
 func main() {
 	var gridData solarmon.DataResponse
@@ -14,6 +24,8 @@ func main() {
 
 	var inverterData solarmon.PerfData
 	inverterChan := make(chan solarmon.PerfData)
+
+	FileWriterLiveDataChan := make(chan LiveData, 50)
 
 	configReader := viper.New()
 	configReader.SetConfigName("solarmon")
@@ -38,25 +50,49 @@ func main() {
 	inv.Host = configReader.GetString("inverter.host")
 	inv.Port = uint16(configReader.GetInt("inverter.port"))
 
-	//gridResult := meter.GetData()
-	//inverterPwr := inv.GetReg("I_AC_Power")
-	//inverterVoltage := inv.GetReg("I_AC_VoltageAB")
-	//inverterCurrent := inv.GetReg("I_AC_Current")
+	j := 0
+	pollms := time.Duration(configReader.GetInt("solarmon.pollInterval")) * time.Millisecond
+	liveFilename := configReader.GetString("solarmon.liveDataFile")
 
-	j:=0
-	pollms := time.Duration(1000 * time.Millisecond)
-	
+	var dataOut LiveData
+
 	for j < 20 {
-	
+
 		go inv.PollData(inverterChan)
 		go meter.PollData(gridChan)
+
+		go FileWriter(liveFilename, FileWriterLiveDataChan)
 
 		gridData = <-gridChan
 		inverterData = <-inverterChan
 
-		fmt.Printf("Grid Demand: %.6g W, Solar Output: %.6g W, House is using: %.6gW\n", gridData.InstantaneousDemand*1000, inverterData.AC_Power, inverterData.AC_Power+(gridData.InstantaneousDemand*1000.0))
+		gridData.InstantaneousDemand = gridData.InstantaneousDemand * 1000 //Convert kW to W
+
+		dataOut.InverterData = inverterData
+		dataOut.GridData = gridData
+		dataOut.InverterEfficiency = 100 * inverterData.AC_Power / inverterData.DC_Power
+		dataOut.HousePowerUsage = inverterData.AC_Power + (gridData.InstantaneousDemand * 1000.0)
+		dataOut.TimeStamp = time.Now().Unix()
+
+		FileWriterLiveDataChan <- dataOut
+		fmt.Printf("Grid Demand: %.6g W, Solar Generation: %.6g W, House Demand: %.6gW\n",
+			gridData.InstantaneousDemand, inverterData.AC_Power, dataOut.HousePowerUsage)
 
 		time.Sleep(pollms)
 		j++
 	}
+}
+
+func FileWriter(filename string, dataChan chan LiveData) {
+
+	for {
+		dataOut := <-dataChan
+		serialized, err := yaml.Marshal(dataOut)
+		if ( err != nil ) {
+			fmt.Errorf("YAML Marshalling error: %s\n",err)
+		} else {
+			ioutil.WriteFile(filename, serialized, 0644)
+		}
+	}
+
 }
