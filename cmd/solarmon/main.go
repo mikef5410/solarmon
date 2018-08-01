ec7c63c0bf895b5b3ae42468c7331bade0ebe95c
Mike Ferrara <mikef@mrf.sonoma.ca.us>
2018-08-01 14:39:24 -0700

    Add Logging to sqlite3

--- cmd/solarmon/main.go
+++ cmd/solarmon/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	//"flag"
+	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/mikef5410/solarmon"
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
@@ -26,6 +28,7 @@ func main() {
 	inverterChan := make(chan solarmon.PerfData)
 
 	FileWriterLiveDataChan := make(chan LiveData, 50)
+	DBWriterChan := make(chan LiveData, 50)
 
 	configReader := viper.New()
 	configReader.SetConfigName("solarmon")
@@ -39,6 +42,8 @@ func main() {
 		panic(fmt.Errorf("Fatal error in config file: %s \n", err))
 	}
 
+	dbFile := configReader.GetString("solarmon.dbFile")
+
 	var meter solarmon.RainforestEagle200Local
 	var inv solarmon.SolarEdgeModbus
 
@@ -50,14 +55,22 @@ func main() {
 	inv.Host = configReader.GetString("inverter.host")
 	inv.Port = uint16(configReader.GetInt("inverter.port"))
 
-	//j := 0
+	//How long to sleep between polls?
 	pollms := time.Duration(configReader.GetInt("solarmon.pollInterval")) * time.Millisecond
+
+	//Where does the live data file live?
 	liveFilename := configReader.GetString("solarmon.liveDataFile")
 
 	var dataOut LiveData
 
+	database := openDB(dbFile)
+
+	//Launch our live data file writer, and database logger threads
 	go FileWriter(liveFilename, FileWriterLiveDataChan)
-	for /*j < 20*/ {
+	go DBWriter(database, DBWriterChan)
+
+	//Loop, polling for data and writing to our filerwriter and dbwriter channels
+	for {
 		stopInv := make(chan int, 1)
 		stopMeter := make(chan int, 1)
 
@@ -91,6 +104,7 @@ func main() {
 			dataOut.TimeStamp = time.Now().Unix()
 
 			FileWriterLiveDataChan <- dataOut
+			DBWriterChan <- dataOut
 			//fmt.Printf("Grid Demand: %.6g W, Solar Generation: %.6g W, House Demand: %.6gW\n",
 			//	gridData.InstantaneousDemand, inverterData.AC_Power, dataOut.HousePowerUsage)
 		} else {
@@ -99,10 +113,10 @@ func main() {
 			stopMeter <- 1
 		}
 		time.Sleep(pollms)
-		//		j++
 	}
 }
 
+//Write the LiveData file for web use
 func FileWriter(filename string, dataChan chan LiveData) {
 
 	for {
@@ -113,6 +127,40 @@ func FileWriter(filename string, dataChan chan LiveData) {
 		} else {
 			ioutil.WriteFile(filename, serialized, 0644)
 		}
+	}
+
+}
+
+func openDB(filename string) *sql.DB {
+	database, _ := sql.Open("sqlite3", filename)
+	statement, _ := database.Prepare(`CREATE TABLE IF NOT EXISTS solarPerf (id INTEGER PRIMARY KEY,
+                        meter_lastContact datetime, meter_demand REAL, meter_KWHFromGrid REAL, meter_KWHToGrid REAL,
+                        inv_AC_Power REAL, inv_AC_Current REAL, inv_AC_Voltage REAL, inv_AC_VA REAL, inv_AC_VAR REAL,
+                        inv_AC_PF REAL, inv_AC_Freq REAL, inv_AC_Energy REAL, inv_DC_Voltage REAL, inv_DC_Current REAL,
+                        inv_DC_Power REAL, inv_SinkTemp REAL, inv_Status int, inv_Event1 int, inv_Efficiency REAL,
+                        House_Demand REAL, timestamp datetime )`)
+	statement.Exec()
+	return (database)
+}
+
+func DBWriter(db *sql.DB, dataChan chan LiveData) {
+	statement, _ := db.Prepare(`INSERT INTO solarPerf (datetime(meter_lastContact,'unixepoch'), meter_demand, meter_KWHFromGrid, meter_KWHToGrid,
+                                    inv_AC_Power, inv_AC_Current, inv_AC_Voltage, inv_AC_VA, inv_AC_VAR, 
+                                    inv_AC_PF, inv_AC_Freq, inv_AC_Energy, inv_DC_Voltage, inv_DC_Current,
+                                    inv_DC_Power, inv_SinkTemp, inv_Status, inv_Event1, inv_Efficiency,
+                                    House_Demand, datetime(timestamp,'unixepoch') ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+
+	for {
+		currentData := <-dataChan
+		statement.Exec(currentData.GridData.LastContact.Unix(),
+			currentData.GridData.InstantaneousDemand, currentData.GridData.KWhFromGrid,
+			currentData.GridData.KWhToGrid, currentData.InverterData.AC_Power, currentData.InverterData.AC_Current,
+			currentData.InverterData.AC_Voltage, currentData.InverterData.AC_VA, currentData.InverterData.AC_VAR,
+			currentData.InverterData.AC_PF, currentData.InverterData.AC_Freq, currentData.InverterData.AC_Energy,
+			currentData.InverterData.DC_Voltage, currentData.InverterData.DC_Current,
+			currentData.InverterData.DC_Power, currentData.InverterData.SinkTemp, int(currentData.InverterData.Status),
+			int(currentData.InverterData.Event1), currentData.InverterEfficiency, currentData.HousePowerUsage,
+			currentData.TimeStamp)
 	}
 
 }
